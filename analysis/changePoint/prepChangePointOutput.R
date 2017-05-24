0ec53415d536ba58fb616047c4045842f301eb5c
Aaron Culich <aculich@gmail.com, aculich@berkeley.edu>
2017-05-23 18:16:17 -0700

    add filename_suffix from env vars SLURM_ARRAY_JOB_ID and SLURM_ARRAY_TASK_ID

--- analysis/changePoint/prepChangePointOutput.R
+++ analysis/changePoint/prepChangePointOutput.R
@@ -8,8 +8,15 @@ print(args)
 w <- 4
 
 samples <- read.csv("../../../data/samples.csv")
-results <- read.table(sprintf('results_%s.txt', w), sep=' ')
-logs <- read.table(sprintf('LogLs_%s.txt', w), sep=' ')
+
+## FIXME: later this should be generalized to passing in the
+## filename instead of explicitly hard-coding SLURM jobarrays, but
+## do what ya gotta do when you're trying to hit a deadline.
+SLURM_ARRAY_JOB_ID  <- Sys.getenv('SLURM_ARRAY_JOB_ID')
+SLURM_ARRAY_TASK_ID <- Sys.getenv('SLURM_ARRAY_TASK_ID')
+filename_suffix     <- paste(SLURM_ARRAY_JOB_ID, SLURM_ARRAY_TASK_ID, sep='_')
+results <- read.table(sprintf('results_%s_%s.txt', w, filename_suffix), sep=' ')
+logs <- read.table(sprintf('LogLs_%s_%s.txt', w, filename_suffix), sep=' ')
 
 chpts <- makeChangepointData(results=results, logs=logs, samples=samples,
                              value=0.949, w=4, file.name=args[1])
